d88f1814ab5742b5ae5a877527c180844d18b652
zhangjianyang <895157882@qq.com>
2017-12-20 17:23:31 +0800

    更新文件

---
+++ refs/web-crawler/src/server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const Crawler = require('./services/crawler');
+
+const app = express();
+const port = 3000;
+
+//set layout
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutDir: path.join(__dirname, 'views/layouts'),
+}));
+
+//set view engine
+app.set('view engine', '.hbs');
+
+//set views path
+app.set('views', path.join(__dirname, 'views'));
+
+//render home view
+app.get('/', (request, response) => {
+    const crawler = new Crawler();
+    crawler.getTaobaoIndexImgs(function (srcs) {
+        response.render('home', {
+            srcs: srcs
+        });
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+    console.log(`server is listening on ${port}`);
+});
+
+app.use((err, request, response, next) => {
+    console.log(err);
+    response.status(500).send('Something broken!');
+});

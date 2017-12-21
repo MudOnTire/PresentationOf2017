2c14585fec73cb373d6575ea7bdbaceebfd1692d
zhangjianyang <895157882@qq.com>
2017-12-21 18:16:59 +0800

    修改文件

--- refs/my-server/server.js
+++ refs/my-server/server.js
@@ -1,26 +1,37 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const Crawler = require('./services/crawler');
+const Crawler = require('./src/services/crawler');
 
 const app = express();
 const port = 3000;
 
 //set layout
 app.engine('.hbs', exphbs({
+    // defaultLayout: path.join(__dirname, 'src/views/layouts/main.hbs'),
     defaultLayout: 'main',
     extname: '.hbs',
-    layoutDir: path.join(__dirname, 'views/layouts'),
+    layoutsDir: path.join(__dirname, 'src/views/layouts')
 }));
 
 //set view engine
 app.set('view engine', '.hbs');
 
 //set views path
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 
-//render crawler view
-app.get('/', (request, response) => {
+//set static files root
+app.use(express.static('src/assets'));
+
+//render start crawler view
+app.get('/startCrawler', (request, response) => {
+    response.render('startCrawler', {});
+});
+
+app.use(express.static(path.join(__dirname, 'src/css/shares')));
+
+//render taobao index images view
+app.get('/taobaoIndexImgs', (request, response) => {
     const crawler = new Crawler();
     crawler.getTaobaoIndexImgs(function (srcs) {
         response.render('crawler', {
@@ -28,8 +39,6 @@ app.get('/', (request, response) => {
         });
     });
 });
-
-
 
 
 
